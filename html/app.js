dbe76f70b822b1f5f5943869366daac87860c5d6
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-05-11 13:46:54 +0200

    chore: format code
    - Use double quotes everywhere in app.js instead of half single and half double.
    - Converts setup function in app.js to use 4 spaces for indent just like the rest of the file.
    - Formats all css code so that each property is defined on a new line.

--- html/app.js
+++ html/app.js
@@ -1,100 +1,100 @@
-const { ref } = Vue
+const { ref } = Vue;
 
 // Customize language for dialog menus and carousels here
 
 const load = Vue.createApp({
-  setup () {
-    return {
-      slides: [
-        {
-            text: "You can add/remove items, vehicles, jobs & gangs through the shared folder.",
-            subText: "Photo captured by: Markyoo#8068",
-            image: "assets/images/1.png",
-        },
-        {
-            text: "Find any issues or want to suggest feature requests? Head to our github and create an issue!",
-            subText: "Photo captured by: ihyajb#9723",
-            image: "assets/images/2.png",
-        },
-        {
-            text: "All server-specific adjustments can be made in the config.lua files throughout the build.",
-            subText: "Photo captured by: FLAPZ[INACTIV]#9925",
-            image: "assets/images/3.png",
-        },
-        {
-            text: "For additional support please join our community at discord.gg/qbox",
-            subText: "Photo captured by: Robinerino#1312",
-            image: "assets/images/4.png",
-        },
-      ],
+    setup() {
+        return {
+            slides: [
+                {
+                    text: "You can add/remove items, vehicles, jobs & gangs through the shared folder.",
+                    subText: "Photo captured by: Markyoo#8068",
+                    image: "assets/images/1.png",
+                },
+                {
+                    text: "Find any issues or want to suggest feature requests? Head to our github and create an issue!",
+                    subText: "Photo captured by: ihyajb#9723",
+                    image: "assets/images/2.png",
+                },
+                {
+                    text: "All server-specific adjustments can be made in the config.lua files throughout the build.",
+                    subText: "Photo captured by: FLAPZ[INACTIV]#9925",
+                    image: "assets/images/3.png",
+                },
+                {
+                    text: "For additional support please join our community at discord.gg/qbox",
+                    subText: "Photo captured by: Robinerino#1312",
+                    image: "assets/images/4.png",
+                },
+            ],
 
-      DownloadTitle: 'Downloading Qbox Server',
-      DownloadDesc: "Hold tight while we begin downloading all the resources/assets required to play on QBox Server. \n\nAfter download has been finished successfully, you'll be placed into the server and this screen will disappear. Please don't leave or turn off your PC. ",
+            DownloadTitle: "Downloading Qbox Server",
+            DownloadDesc: "Hold tight while we begin downloading all the resources/assets required to play on QBox Server. \n\nAfter download has been finished successfully, you'll be placed into the server and this screen will disappear. Please don't leave or turn off your PC. ",
 
-      SettingsTitle: 'Settings',
-      AudioTrackDesc1: 'When disabled the current audio-track playing will be stopped.',
-      AutoPlayDesc2: 'When disabled carousel images will stop cycling and remain on the last shown.',
-      PlayVideoDesc3: 'When disabled video will stop playing and remain paused.',
+            SettingsTitle: "Settings",
+            AudioTrackDesc1: "When disabled the current audio-track playing will be stopped.",
+            AutoPlayDesc2: "When disabled carousel images will stop cycling and remain on the last shown.",
+            PlayVideoDesc3: "When disabled video will stop playing and remain paused.",
 
-      KeybindTitle: 'Default Keybinds',
+            KeybindTitle: "Default Keybinds",
 
-      keybinds: [
-        [
-            { key: "TAB", label: "Open Inventory"},
-            { key: "`", label: "Cycle Proximity"},
-            { key: "M", label: "Open Phone"},
-            { key: "B", label: "Toggle Seat Belt"},
-            { key: "L ALT", label: "Open Target Menu"},
-            { key: "F1", label: "Radial Menu"},
-            { key: "I", label: "Open Hud Menu"},
-            { key: "L ALT", label: "Talk Over Radio"},
-        ],
-        [
-            { key: "L", label: "Vehicle Locks"},
-            { key: "G", label: "Toggle Engine"},
-            { key: "B", label: "Pointer Emote"},
-            { key: "HOME", label: "Open Scoreboard"},
-            { key: "Z", label: "Keybind Slots"},
-            { key: "X", label: "Hands Up Emote"},
-            { key: "NUM", label: "Use Item Slots"},
-            { key: "Y", label: "Cruise Control"},
-        ],
-      ],
+            keybinds: [
+                [
+                    { key: "TAB", label: "Open Inventory" },
+                    { key: "`", label: "Cycle Proximity" },
+                    { key: "M", label: "Open Phone" },
+                    { key: "B", label: "Toggle Seat Belt" },
+                    { key: "L ALT", label: "Open Target Menu" },
+                    { key: "F1", label: "Radial Menu" },
+                    { key: "I", label: "Open Hud Menu" },
+                    { key: "L ALT", label: "Talk Over Radio" },
+                ],
+                [
+                    { key: "L", label: "Vehicle Locks" },
+                    { key: "G", label: "Toggle Engine" },
+                    { key: "B", label: "Pointer Emote" },
+                    { key: "HOME", label: "Open Scoreboard" },
+                    { key: "Z", label: "Keybind Slots" },
+                    { key: "X", label: "Hands Up Emote" },
+                    { key: "NUM", label: "Use Item Slots" },
+                    { key: "Y", label: "Cruise Control" },
+                ],
+            ],
 
-      firstap: ref(true),
-      secondap: ref(true),
-      thirdap: ref(true),
-      firstslide: ref(1),
-      secondslide: ref(1),
-      thirdslide: ref(1),
-      audioplay: ref(true),
-      playvideo: ref(true),
-      download: ref(true),
-      settings: ref(false),
-    }
-  }
-})
+            firstap: ref(true),
+            secondap: ref(true),
+            thirdap: ref(true),
+            firstslide: ref(1),
+            secondslide: ref(1),
+            thirdslide: ref(1),
+            audioplay: ref(true),
+            playvideo: ref(true),
+            download: ref(true),
+            settings: ref(false),
+        };
+    },
+});
 
-load.use(Quasar, { config: {} })
-load.mount('#loading-main')
+load.use(Quasar, { config: {} });
+load.mount("#loading-main");
 
 $(document).ready(function () {
     var audioFiles = [
-        'assets/audio/ambientgold.mp3',
-        'assets/audio/chimes.mp3',
-        'assets/audio/daze.mp3',
-        'assets/audio/galaxy.mp3',
-        'assets/audio/highwaynights.mp3',
-        'assets/audio/meteorbinge.mp3'
+        "assets/audio/ambientgold.mp3",
+        "assets/audio/chimes.mp3",
+        "assets/audio/daze.mp3",
+        "assets/audio/galaxy.mp3",
+        "assets/audio/highwaynights.mp3",
+        "assets/audio/meteorbinge.mp3",
     ];
 
     var randomSong = audioFiles[Math.floor(Math.random() * audioFiles.length)];
 
-    $('#audio').attr('src', randomSong);
+    $("#audio").attr("src", randomSong);
 
-    $('#audio')[0].autoplay = true;
+    $("#audio")[0].autoplay = true;
 
-    $('#audio')[0].play();
+    $("#audio")[0].play();
 });
 
 var audio = document.getElementById("audio");
